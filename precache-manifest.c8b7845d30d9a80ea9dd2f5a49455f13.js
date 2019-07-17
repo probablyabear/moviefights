cbbd6623374e1f87457149645c4060ff979839e1
Stephen Rust <rust.stephen@gmail.com>
2019-07-17 01:55:18 -0500

    Updates

---
+++ precache-manifest.c8b7845d30d9a80ea9dd2f5a49455f13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a51461a020c728546d36cd55d5210e60",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "2ba58d552c5b2dce67bf",
+    "url": "/my-app/static/css/main.ba46162b.chunk.css"
+  },
+  {
+    "revision": "f50ab16afb837c020e09",
+    "url": "/my-app/static/js/2.7a3a4b61.chunk.js"
+  },
+  {
+    "revision": "2ba58d552c5b2dce67bf",
+    "url": "/my-app/static/js/main.46e91536.chunk.js"
+  },
+  {
+    "revision": "9930044c31793394defe",
+    "url": "/my-app/static/js/runtime~main.6019dd84.js"
+  },
+  {
+    "revision": "e8278b943251d8e0094840591e10644d",
+    "url": "/my-app/static/media/fight-white.e8278b94.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f338fc1b54e10cb0900ce2243011eb5a.js"
+  "/my-app/precache-manifest.c8b7845d30d9a80ea9dd2f5a49455f13.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
